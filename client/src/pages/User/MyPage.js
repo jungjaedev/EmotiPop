aaee7230b07f8b0da36e055e50b1fb40399761c3
ajrfyd <ajrfyd@naver.com>
2021-12-10 21:15:18 +0900

    tmp2

--- client/src/pages/User/MyPage.js
+++ client/src/pages/User/MyPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { 
     Button, 
     View, 
@@ -12,45 +12,13 @@ import {reqSignOut, reSignIn} from '../../modules/user';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components/native';
 import Nav from '../Home/Nav'
-import { TextInput } from 'react-native-gesture-handler';
-import axios from 'axios';
 
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 
-export default function MyPage() {
-  const [editInfoForm, setEditInfoForm] = useState({
-    username: '',
-    email: '',
-    password: '',
-    repassword: ''
-  })
-  const {username, email, password, repassword} = editInfoForm;
-  const token = useSelector(state => state.user.accessToken);
-  function changeDetector(e) {
-    const { name, value } = e;
-    setEditInfoForm({
-      ...editInfoForm,
-      [name]: value
-    })
-  }
-  console.log(editInfoForm)
-  function submitForm(e) {
-    e.preventDefault()
-
-    axios.patch('http://localhost:80/mypage/me', {
-      username, email, password
-    }, { headers: {
-      'ContentType': 'application/json',
-      authorization: `Bearer ${token}`
-    }, 'withCredentials': true
-    }).then(data => {
-      console.log(data)
-    })
-    console.log(editInfoForm)
-  }
-
+export default function MyPage({navigation}) {
+  const dispatch = useDispatch();
 
   const logOutHandler = async () => {
     // console.log('ads')
@@ -59,7 +27,7 @@ export default function MyPage() {
     // AsyncStorage.clear()
     // const res = await AsyncStorage.getAllKeys()
     // location.reload()
-    // Update.reloadAsync()
+    Update.reloadAsync()
     // console.log(res)
   }
   
@@ -81,24 +49,121 @@ export default function MyPage() {
       height: SCREEN_HEIGHT - 60
     }}>
       <Text style={{flex: 1}}>MyPage</Text>
-      <TouchableOpacity style={{flex: 4}} onPress={logOutHandler()}>
+      <TouchableOpacity style={{flex: 4}} onPress={logOutHandler}>
         <Text>SignOut</Text>
       </TouchableOpacity>
-      <UserInfomation>
-        <TextInput onPress={changeDetector()}>이름 : {username}</TextInput>
-        <TextInput>이메일 : {email}</TextInput>
-        <TextInput>비밀번호 : {password}</TextInput>
-        <TextInput>비밀번호 확인 : {repassword}</TextInput>
-        <TouchableOpacity onPress={() => submitForm()} />
-      </UserInfomation>
-      <Nav />
+      {/* <Nav /> */}
     </View>
   );
 }
 
-const UserInfomation = styled.View`
-  flex: auto;
-  background-color: ivory;
-  height: 200px;
-  width: 200px;
-`
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import { 
+//     Button, 
+//     View, 
+//     Text,
+//     TouchableOpacity,
+//     Dimensions,
+//   } from 'react-native';
+// import AsyncStorage from '@react-native-async-storage/async-storage'; 
+// import * as Update from "expo-updates";
+// import {reqSignOut, reSignIn} from '../../modules/user';
+// import { useSelector, useDispatch } from 'react-redux';
+// import styled from 'styled-components/native';
+// import Nav from '../Home/Nav'
+// import { TextInput } from 'react-native-gesture-handler';
+// import axios from 'axios';
+
+
+// const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+
+// export default function MyPage() {
+//   const [editInfoForm, setEditInfoForm] = useState({
+//     username: '',
+//     email: '',
+//     password: '',
+//     repassword: ''
+//   })
+//   const {username, email, password, repassword} = editInfoForm;
+//   const token = useSelector(state => state.user.accessToken);
+//   function changeDetector(e) {
+//     const { name, value } = e;
+//     setEditInfoForm({
+//       ...editInfoForm,
+//       [name]: value
+//     })
+//   }
+//   console.log(editInfoForm)
+//   function submitForm(e) {
+//     e.preventDefault()
+
+//     axios.patch('http://localhost:80/mypage/me', {
+//       username, email, password
+//     }, { headers: {
+//       'ContentType': 'application/json',
+//       authorization: `Bearer ${token}`
+//     }, 'withCredentials': true
+//     }).then(data => {
+//       console.log(data)
+//     })
+//     console.log(editInfoForm)
+//   }
+
+
+//   const logOutHandler = async () => {
+//     // console.log('ads')
+//     const token = await AsyncStorage.getItem('AccessToken')
+//     dispatch(reqSignOut(token))
+//     // AsyncStorage.clear()
+//     // const res = await AsyncStorage.getAllKeys()
+//     // location.reload()
+//     // Update.reloadAsync()
+//     // console.log(res)
+//   }
+  
+//   //! Token owner Test
+//   // const logg = async() => {
+//   //   try {
+//   //     const token = await AsyncStorage.getItem('AccessToken')
+//   //     dispatch(reSignIn(token))
+//   //   } catch(err) {
+//   //   throw new Error(err)
+//   //   }
+//   // }
+
+//   return (
+//     <View style={{ 
+//       alignItems: 'center', 
+//       justifyContent: 'space-between',
+//       width: SCREEN_WIDTH,
+//       height: SCREEN_HEIGHT - 60
+//     }}>
+//       <Text style={{flex: 1}}>MyPage</Text>
+//       <TouchableOpacity style={{flex: 4}} onPress={logOutHandler()}>
+//         <Text>SignOut</Text>
+//       </TouchableOpacity>
+//       <UserInfomation>
+//         <TextInput onPress={changeDetector()}>이름 : {username}</TextInput>
+//         <TextInput>이메일 : {email}</TextInput>
+//         <TextInput>비밀번호 : {password}</TextInput>
+//         <TextInput>비밀번호 확인 : {repassword}</TextInput>
+//         <TouchableOpacity onPress={() => submitForm()} />
+//       </UserInfomation>
+//       <Nav />
+//     </View>
+//   );
+// }
+
+// const UserInfomation = styled.View`
+//   flex: auto;
+//   background-color: ivory;
+//   height: 200px;
+//   width: 200px;
+// `
+
