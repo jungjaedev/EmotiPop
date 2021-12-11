21b4c6a6911c26f7429620b50b75137a405e121a
Jaewon <jungjaedev@gmail.com>
2021-12-11 21:08:06 +0900

    Fix: [Op.and] syntax

--- server/controller/Calendar/BeansList.js
+++ server/controller/Calendar/BeansList.js
@@ -4,56 +4,58 @@ const sequelize = require('sequelize');
 const { Op } = require('sequelize');
 
 module.exports = {
-    get: async (req, res) => {
-        if (!req.headers['authorization']) {
-            return res.status(401).send({
-            message: 'invalid access token',
-            });
-        }
+  get: async (req, res) => {
+    if (!req.headers['authorization']) {
+      return res.status(401).send({
+        message: 'invalid access token',
+      });
+    }
 
-        const accessTokenData = isAuthorized(req);
+    const accessTokenData = isAuthorized(req);
 
-        const userInfo = await Users.findOne({
-            where: {
-                email: accessTokenData.email
-            },
-        });
-        // console.log(userInfo);
-        if (!userInfo) {
-            res.status(403).send({
-                message: 'Forbidden',
-            });
-        }
+    const userInfo = await Users.findOne({
+      where: {
+        email: accessTokenData.email,
+      },
+    });
+    // console.log(userInfo);
+    if (!userInfo) {
+      res.status(403).send({
+        message: 'Forbidden',
+      });
+    }
     const users_id = userInfo.dataValues.id;
 
     const date = req.params.calendar_id;
-    const  { gourdKinds } = req.params;
-    // console.log(gourdKind);
+    const { gourdKinds } = req.params;
+    // console.log(users_id);
+    // console.log(date);
 
     const pickYear = date.split('-')[0];
     const pickMonth = date.split('-')[1];
     const pickDate = date.split('-')[2];
     const beansList = await Beans.findAll({
-        where: {
-            [Op.and]: [sequelize.where(sequelize.fn('year', sequelize.col('createdAt')), pickYear)],
-            [Op.and]: [sequelize.where(sequelize.fn('month', sequelize.col('createdAt')), pickMonth)],
-            [Op.and]: [sequelize.where(sequelize.fn('day', sequelize.col('createdAt')), pickDate)],
-            [Op.and]: { users_id },
-            [Op.and]: { gourdKinds },
-        }
+      where: {
+        [Op.and]: [
+          sequelize.where(sequelize.fn('year', sequelize.col('createdAt')), pickYear),
+          sequelize.where(sequelize.fn('month', sequelize.col('createdAt')), pickMonth),
+          sequelize.where(sequelize.fn('day', sequelize.col('createdAt')), pickDate),
+          { users_id },
+          { gourdKinds },
+        ],
+      },
     });
 
     const beansArray = [];
 
     beansList.map(beans => {
-        // console.log(beans.dataValues);
-        beansArray.push(beans.dataValues);
+      // console.log(beans.dataValues);
+      beansArray.push(beans.dataValues);
     });
-
+    // console.log(beansArray);
     res.status(200).json({
-        message: 'beanslist get completed',
-        data: beansArray
-    })
-        
-    }
-}
+      message: 'beanslist get completed',
+      data: beansArray,
+    });
+  },
+};
